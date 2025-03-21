a72e9fd434da6f1a8d7115f6cac25daae032d555
tat <tatsu0823takasago@icloud.com>
2025-03-22 01:53:50 +0900

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      // ここにオプションを指定可能。通常は空でOK
-    }),
-  ],
-  output: 'server',   // SSRモードは維持
-  adapter: netlify(),  // Netlify用アダプターに変更
+  integrations: [react()],
+  output: 'static'  // SSRからstatisticに変更
 });
