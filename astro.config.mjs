7eaa6a47a8d00596407b48a428498c1ab9ed1478
tat <tatsu0823takasago@icloud.com>
2025-03-22 02:13:54 +0900

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
