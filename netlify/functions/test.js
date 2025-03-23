c8b7c11566b82f6e0a593a026ba33a3abbe5bb72
tat <tatsu0823takasago@icloud.com>
2025-03-23 14:51:28 +0900

    new

--- netlify/functions/test.js
+++
@@ -1,7 +0,0 @@
-// テスト用関数 - Netlify Functionsが正しく動作するか確認
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Netlify Functions テスト成功" })
-  };
-}; 
